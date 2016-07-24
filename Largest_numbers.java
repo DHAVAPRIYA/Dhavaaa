79f35b85d0a4c0c254beccf257f64bfd974062a7
DHAVAPRIYA <thavapriya96@gmail.com>
2016-07-24 10:51:05 +0530

    Add files via upload

---
+++ Largest_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Largest_numbers {
+public static void main(String[] args) {
+	int a,b,c;
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the three numbers:");
+	a=in.nextInt();
+	b=in.nextInt();
+	c=in.nextInt();
+	if(a>b&&b>c)
+	{
+		System.out.println("A is big");
+	}
+	else if(b>c)
+	{
+		System.out.println("B is big");
+	}
+	else
+		System.out.println("C is big");
+}
+}
