0ca092c953a510fccdba69447f6fddaf2d29db5a
DHAVAPRIYA <thavapriya96@gmail.com>
2016-09-04 16:50:40 +0530

    Add files via upload

--- Integer.java
+++ Integer.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 
 public class Integer {
 public static void main(String[] args) {
+	int decimal = 0;
 	Scanner in = new Scanner(System.in);
-	System.out.println("Enter the Reverse number : ");
-	int n = in.nextInt();
-	String str = n+"";
-	StringBuffer br = new StringBuffer(str);
-	br.reverse();
-	String s = br.toString();
-	System.out.println(s);
-}
+	String str = in.nextLine();
+    String str1 = str.toUpperCase();
+    for(int x = 0;x<str1.length();x++)
+    {
+        char ch = str.charAt(x);
+        switch (ch)
+        {
+        case 'X':
+            decimal += 10;
+            break;
+
+        case 'V':
+            decimal += 5;
+            break;
+
+        case 'I':
+            decimal += 1;
+            break;
+        }
+    }
+  System.out.println(decimal);
+}	
 
 }
